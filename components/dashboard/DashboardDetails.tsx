a0f0bc6806d8262b4c421c2fd49746ce0aba07cd
irakibul7 <irakibul568@gmail.com>
2026-07-21 12:35:47 +0600

    Add live AI pilot readiness card

--- components/dashboard/DashboardDetails.tsx
+++ components/dashboard/DashboardDetails.tsx
@@ -2,6 +2,7 @@ import { getAdminOverviewAction } from "@/app/actions/admin";
 import RecentClients from "./RecentClients";
 import TopCardItem from "./TopCardItem";
 import TopHeader from "./TopHeader";
+import LiveAiPilotCard from "./LiveAiPilotCard";
 
 const DashboardDetails = async () => {
   const response = await getAdminOverviewAction();
@@ -12,6 +13,7 @@ const DashboardDetails = async () => {
     <div className="space-y-8 px-6">
       <TopHeader />
       <TopCardItem totals={totals} />
+      <LiveAiPilotCard />
       <RecentClients clients={latestClients} />
     </div>
   );
